db4edc93323398770e9e85c85dc30cefa6048e0b
* <apolverini@yahoo.it>
2018-06-17 09:38:41 +0200

    added app

---
+++ app/src/main/java/a/polverini/my/MainActivity.java
@@ -0,0 +1,14 @@
+package a.polverini.my;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
